d0252fda3cbac231b2f9b98d028fca2c1692fa28
guoming <guoming@datangnet.com.cn>
2019-04-29 14:05:04 +0800

    roll back

--- peers-lib/src/main/java/net/sourceforge/peers/media/RtpSender.java
+++ peers-lib/src/main/java/net/sourceforge/peers/media/RtpSender.java
@@ -93,7 +93,7 @@ public class RtpSender implements Runnable {
         int sequenceNumber = random.nextInt();
         rtpPacket.setSequenceNumber(sequenceNumber);
         rtpPacket.setSsrc(random.nextInt());
-        int buf_size = Capture.BUFFER_SIZE ;
+        int buf_size = Capture.BUFFER_SIZE / 2;
         byte[] buffer = new byte[buf_size];
         int timestamp = 0;
         int numBytesRead;
