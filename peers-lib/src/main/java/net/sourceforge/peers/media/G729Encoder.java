b295f69edcf49a949df05a29b91235703013f0f6
luxuedong <me@luxuedong.com>
2019-07-11 12:57:31 +0800

    G729 commit

--- peers-lib/src/main/java/net/sourceforge/peers/media/G729Encoder.java
+++ peers-lib/src/main/java/net/sourceforge/peers/media/G729Encoder.java
@@ -5,6 +5,7 @@ import org.restcomm.media.spi.memory.Frame;
 import org.restcomm.media.spi.memory.Memory;
 
 import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
 
 /**
  * Created by luxuedong
@@ -26,9 +27,32 @@ public class G729Encoder extends Encoder{
     public byte[] process(byte[] media) {
         logger.debug("+++++++++++++++++++PCM Encoder To G729 Before length:" + media.length);
 //        byte[] bytes = pcm2g729(media);
-        byte[] bytes = encoder.process(media);
+        byte[] bytes = encodeByte(media);
         logger.debug("+++++++++++++++++++PCM Encoder To G729 after length:" + bytes.length);
         return bytes;
+    }
+
+    public byte[] encodeByte(byte[] bytes) {
+        byte[] bb = new byte[bytes.length / 16];
+        ArrayList<Byte> list = new ArrayList<>();//没有解压的集合
+        ArrayList<Byte> list2 = new ArrayList<>();//解压完的集合
+        for (int i = 0; i < bytes.length; i++) {
+            list.add(bytes[i]);//为集合添加数据
+        }
+        for (int i = 0; i < list.size() / 160; i++) {
+            byte[] b = new byte[160];//创建160为基准的小byte【】
+            for (int j = 0; j < 160; j++) {
+                b[j] = list.get(i * 160 + j);
+            }
+            byte[] process = encoder.process(b);
+            for (int j = 0; j < process.length; j++) {
+                list2.add(process[j]);
+            }
+        }
+        for (int i = 0; i < list2.size(); i++) {
+            bb[i] = list2.get(i);
+        }
+        return bb;
     }
 
     public byte[] pcm2g729(byte[] data) {
