2aacc1051c586c6b6f6624294420dcf330d22aa5
luxuedong <me@luxuedong.com>
2019-07-30 11:22:23 +0800

    G729 commit

--- peers-lib/src/main/java/net/sourceforge/peers/sdp/SDPManager.java
+++ peers-lib/src/main/java/net/sourceforge/peers/sdp/SDPManager.java
@@ -49,10 +49,18 @@ public class SDPManager {
         random = new Random();
         //TODO retrieve codecs from configuration file
 
-        Codec codec = new Codec();
-        codec.setPayloadType(RFC3551.PAYLOAD_TYPE_G729);
-        codec.setName(RFC3551.G729);
-        supportedCodecs.add(codec);
+        Config config = userAgent.getConfig();
+        String iscodecstr = config.getCustomSipHeaders().get("X-DTai-ISCodec");
+
+        int iscodec = Integer.parseInt(iscodecstr);
+
+        Codec codec = null;
+        if(iscodec==1){
+            codec = new Codec();
+            codec.setPayloadType(RFC3551.PAYLOAD_TYPE_G729);
+            codec.setName(RFC3551.G729);
+            supportedCodecs.add(codec);
+        }
         codec = new Codec();
         codec.setPayloadType(RFC3551.PAYLOAD_TYPE_PCMU);
         codec.setName(RFC3551.PCMU);
